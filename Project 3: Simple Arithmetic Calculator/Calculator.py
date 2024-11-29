67a5da5f85df9826081832b5686ff721b4686ec1
Pakhi Jain <pakhijain2703@gmail.com>
2024-11-29 22:58:57 +0530

    Update Calculator.py

--- Project 3: Simple Arithmetic Calculator/Calculator.py
+++ Project 3: Simple Arithmetic Calculator/Calculator.py
@@ -10,26 +10,26 @@ choice = int(input("Choose the operation: "))
 # Check if the choice is a valid option (1-4)
 if (choice in(1,2,3,4)):
   # if the choice is valid then promt the user to enter two numbers
-  num1 = int(input("enter first number: "))
-  num2 = int(input("enter second number: "))
-  
+  num1 = float(input("enter first number: "))
+  num2 = float(input("enter second number: "))
+
 # Perform the operation based on the user's choice
 if choice == 1:
   #Addition Operation
   print("Answer = ",num1+num2)
-  
+
 elif choice == 2:
   #Addition Operation
   print("Answer = ",num1-num2)
-  
+
 elif choice == 3:
   #Addition Operation
   print("Answer = ",num1*num2)
-  
+
 elif choice == 4:
   #Addition Operation
   print("Answer = ",num1/num2)
-  
+
 # If the user enters an invalid operation number, print an error message
 else:
   print("Invalid choice")
